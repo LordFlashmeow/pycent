d599da552af995c8a1430aab2907fea958290f49
Dash Peters <dpeters19@bayschoolsf.org>
2017-10-01 15:29:33 -0700

    Add documentation

--- pycent.py
+++ pycent.py
@@ -3,8 +3,45 @@ class pycent:
         pass
 
     def percent_of(self, percent, whole):
+        """Calculates the value of a percent of a number
+        ie: 5% of 20 is what --> 1
+        
+        Args:
+            percent (float): The percent of a number
+            whole (float): The whole of the number
+            
+        Returns:
+            float: The value
+            
+        Example:
+        >>> percent_of(25, 100)
+        25.0
+        >>> percent_of(5, 20)
+        1.0
+        
+        """
         return (percent * whole) / 100
 
     def percentage(self, part, whole):
+        """Calculates the percentage of a number, given a part and a whole
+        ie: 5 is what percent of 20 --> 25
+
+        Args:
+            part (float): The part of a number
+            whole (float): The whole of a number
+
+        Returns:
+            float: The percentage
+
+        Examples:
+        >>> percentage(10, 25)
+        40.0
+        >>> percentage(5, 20)
+        25.0
+
+        """
+        if whole == 0:
+            raise ZeroDivisionError
+
         return 100 * float(part)/float(whole)
 
