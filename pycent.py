2080f0d007f985f05453d091427c5038e9663e97
Dash Peters <dpeters19@bayschoolsf.org>
2017-10-01 15:00:42 -0700

    Create core of pycent

---
+++ pycent.py
@@ -0,0 +1,10 @@
+class pycent:
+    def __init__(self):
+        pass
+
+    def percent_of(self, percent, whole):
+        return (percent * whole) * 100
+
+    def percentage(self, part, whole):
+        return 100 * float(part)/float(whole)
+
