65e3a7be5110a3ef576ea71f2e37181da3d3643d
Jay Turner <jaynicholasturner@gmail.com>
2017-10-04 10:02:40 +0100

    Ensure type hinting and enabled rounding for percentage

--- pycent.py
+++ pycent.py
@@ -1,8 +1,12 @@
+from typing import Union
+
+FloatIn = Union[int, float, str]
+
 class pycent:
     def __init__(self):
         pass
 
-    def percent_of(self, percent, whole):
+    def percent_of(self, percent: FloatIn, whole: FloatIn):
         """Calculates the value of a percent of a number
         ie: 5% of 20 is what --> 1
         
@@ -22,13 +26,14 @@ class pycent:
         """
         return (percent * whole) / 100
 
-    def percentage(self, part, whole):
+    def percentage(self, part: FloatIn, whole: FloatIn, resolution: int=2):
         """Calculates the percentage of a number, given a part and a whole
         ie: 5 is what percent of 20 --> 25
 
         Args:
             part (float): The part of a number
             whole (float): The whole of a number
+			resolution (integer): How many decimal points you want (Defaults to 2)
 
         Returns:
             float: The percentage
@@ -36,12 +41,11 @@ class pycent:
         Examples:
         >>> percentage(10, 25)
         40.0
-        >>> percentage(5, 20)
-        25.0
+        >>> percentage(5, 19, 3)
+        26.316
 
         """
         if whole == 0:
             raise ZeroDivisionError
 
-        return 100 * float(part)/float(whole)
-
+        return round(100 * float(part)/float(whole), resolution)
