53350571e85769027a6f437ae54e4b3b5c0c7429
Dash Peters <dpeters19@bayschoolsf.org>
2017-10-06 21:22:50 +0000

    Add minimum version for package

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 setup(
     name='pycent',
     description='Calculate the percentage of a value, or the reverse',
+    python_requires=">=3.5",
     url='http://github.com/LordFlashmeow/pycent',
     author='LordFlashmeow',
     license='MIT',
