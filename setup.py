bae570367e3184235dc69027a3404acf7be6bb56
Dash Peters <dpeters19@bayschoolsf.org>
2017-10-06 21:06:17 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='pycent',
+    description='Calculate the percentage of a value, or the reverse',
+    url='http://github.com/LordFlashmeow/pycent',
+    author='LordFlashmeow',
+    license='MIT',
+    packages=['pycent'],
+    zip_safe=False
+)
