5a84b150bcf49b3c95be5ca230708e02bdf4d9e7
Dash Peters <dpeters19@bayschoolsf.org>
2017-10-06 21:26:06 +0000

    Add version number

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 setup(
     name='pycent',
+    version="0.1",
     description='Calculate the percentage of a value, or the reverse',
     python_requires=">=3.5",
     url='http://github.com/LordFlashmeow/pycent',
