b7324c7df46ce2f049a0af36d14af9e99817b517
Dash Peters <dpeters19@bayschoolsf.org>
2017-10-07 06:12:09 +0000

    Reorganize setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup
+
 setup(
     name='pycent',
     version="0.1",
     description='Calculate the percentage of a value, or the reverse',
+    author='LordFlashmeow',
+    author_email='lordflashmeow@gmail.com',
+    url='http://github.com/LordFlashmeow/pycent',
+    license='MIT',
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
         'Topic :: Scientific/Engineering :: Mathematics'
         ],
+    keywords='percent percentage math'
     python_requires=">=3.5",
-    url='http://github.com/LordFlashmeow/pycent',
-    author='LordFlashmeow',
-    author_email='lordflashmeow@gmail.com',
-    license='MIT',
     packages=['pycent'],
     zip_safe=True,
-    keywords='percent percentage math'
 )
